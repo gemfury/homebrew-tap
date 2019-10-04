3c264478bddf5737e03a085d1145a2c002bce67e
Mark John Buenconsejo <hi@markjeee.com>
2019-10-04 14:46:12 +0800

    Added new release v0.11.0.rc1.

--- Formula/gemfury.rb
+++ Formula/gemfury.rb
@@ -5,8 +5,8 @@ class Gemfury < Formula
   sha256 "255bf8b02bac91047df169d119101c6134f1d48c54ca4e318f87125316bb2276"
 
   devel do
-    url "https://github.com/gemfury/gemfury/releases/download/v0.10.0/gemfury-0.10.0.tar.gz"
-    sha256 "255bf8b02bac91047df169d119101c6134f1d48c54ca4e318f87125316bb2276"
+    url "https://github.com/gemfury/gemfury/releases/download/v0.11.0.rc1/gemfury-0.11.0.rc1.tar.gz"
+    sha256 "a8c9f99a408c5aa4dfa1f94d9653cdaed600df1157ebcf4d3e342a2925b3d377"
   end
 
   def package_name
