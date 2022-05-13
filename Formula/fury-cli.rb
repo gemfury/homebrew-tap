ced939612878cf5d24f73fc34b008d9d01971f6a
goreleaser <goreleaser@gemfury.com>
2022-05-13 15:54:08 +0800

    Brew formula update for fury version v0.20.0-beta.3

--- Formula/fury-cli.rb
+++ Formula/fury-cli.rb
@@ -10,7 +10,7 @@ class FuryCli < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/gemfury/cli/releases/download/v0.20.0-beta.3/fury_0.20.0-beta.3_macOS_ARM64.tar.gz"
-      sha256 "7e8cebebe338fc6dedb764f914c12aba96958db677bd2fa2b9a9c8ecd473f056"
+      sha256 "bf2550c0637d338f38ad705298a2d497c7a9e338b92051f8e80c7b0311e2929c"
 
       def install
         bin.install "fury"
@@ -18,7 +18,7 @@ class FuryCli < Formula
     end
     if Hardware::CPU.intel?
       url "https://github.com/gemfury/cli/releases/download/v0.20.0-beta.3/fury_0.20.0-beta.3_macOS_64bit.tar.gz"
-      sha256 "fddd4dbdfd20db90b36b6b4e5385b5c207bbbd906f7ddc7d02d2ac9fdc69b0a0"
+      sha256 "ebff47137c5327602589249233eee2b41d3e3573a7a2143365b994444b75eca8"
 
       def install
         bin.install "fury"
@@ -29,7 +29,7 @@ class FuryCli < Formula
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/gemfury/cli/releases/download/v0.20.0-beta.3/fury_0.20.0-beta.3_Linux_64bit.tar.gz"
-      sha256 "9ce89d6d01b37b05303c952820081475db8ce67f34af4d2ba40c35e9ac6eb0a1"
+      sha256 "4bbb05f55a5bbb26ec58adf26baf01ad6b1aeef4b55b736d9f01051e5f5af4ee"
 
       def install
         bin.install "fury"
@@ -37,7 +37,7 @@ class FuryCli < Formula
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/gemfury/cli/releases/download/v0.20.0-beta.3/fury_0.20.0-beta.3_Linux_ARM64.tar.gz"
-      sha256 "719a06a1a147d309288a97e5ec2d2fa38bbbbf6c34daa0939c440bda263594cd"
+      sha256 "4c23ca6f49f255e922fb9834b6beb43c8fedc7a33677db1c22694cac8a0f6826"
 
       def install
         bin.install "fury"
